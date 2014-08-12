9e6cbb793b0b6f7b1c2e9a5bce7e8c5ff44df6a1
Bertrand Rothen <bertrand.rothen@gmail.com>
2014-08-12 23:22:02 +0200

    Signup Setup

--- Gemfile
+++ Gemfile
@@ -24,6 +24,7 @@ gem 'bootstrap-sass', '~> 3.2.0'
 
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
+gem 'devise'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
 gem 'turbolinks'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,6 +32,9 @@ Rails.application.configure do
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
 
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  #In production, :host should be set to the actual host of your application.
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
